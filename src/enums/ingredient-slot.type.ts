5c7f78285e1dc4fdf62456ca2570dddacfb23a02
Javiink <javihamty@gmail.com>
2024-06-18 17:27:29 +0200

    ✨ Basic ingredient setup

---
+++ src/enums/ingredient-slot.type.ts
@@ -0,0 +1,7 @@
+export enum IngredientSlots {
+  'base',
+  'sauce',
+  'protein',
+  'topping',
+  'sprinkle',
+}
---
+++ src/ingredients/entities/ingredient.entity.ts
@@ -0,0 +1,28 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IngredientSlots } from 'src/enums/ingredient-slot.type';
+
+export type IngredientDocument = Ingredient & Document;
+
+@Schema()
+export class Ingredient {
+  @Prop()
+  name: string;
+
+  @Prop()
+  slot: IngredientSlots;
+
+  @Prop()
+  allergens?: string[];
+
+  @Prop()
+  description?: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price?: number;
+}
+
+export const IngredientSchema = SchemaFactory.createForClass(Ingredient);
