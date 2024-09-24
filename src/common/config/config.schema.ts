3c5945e55094c2a1b72e0f0887cf6b2e5b712cbc
Javiink <javihamty@gmail.com>
2024-09-24 12:02:57 +0200

    🔧 Change environment variables validator config to support dockerizing

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -1,21 +1,42 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
+  
   //APP
   NODE_ENV: Joi.string()
     .valid('development', 'staging', 'production')
-    .default('development'),
-  APP_PORT: Joi.number().default(3000),
-  CORS_HOST: Joi.string().default(''),
-  IMAGES_PATH: Joi.string().default('public/images'),
+    .allow('', null)
+    .empty(['', null])
+    .default('production'),
+  APP_PORT: Joi.number().allow('', null).empty(['', null]).default(3000),
+  CORS_HOST: Joi.string().allow('', null).empty(['', null]).default(''),
+  IMAGES_PATH: Joi.string()
+    .allow('', null)
+    .empty(['', null])
+    .default('public/images'),
+
   //DATABASE
   MONGODB_PROTOCOL: Joi.string()
     .valid('mongodb', 'srv', 'mongodb+srv')
+    .allow('', null)
+    .empty(['', null])
     .default('mongodb'),
-  MONGODB_USERNAME: Joi.string().empty(''),
-  MONGODB_PASSWORD: Joi.string().empty(''),
-  MONGODB_HOST: Joi.string().required(),
-  MONGODB_DATABASE: Joi.string().required(),
-  MONGODB_PORT: Joi.number().default(27017),
-  MONGODB_PARAMS: Joi.string().optional(),
+  MONGODB_USERNAME: Joi.string()
+    .allow('', null)
+    .empty(['', null])
+    .default('fastpoke'),
+  MONGODB_PASSWORD: Joi.string()
+    .allow('', null)
+    .empty(['', null])
+    .default('Ch4nG3m3pL34se'),
+  MONGODB_HOST: Joi.string()
+    .allow('', null)
+    .empty(['', null])
+    .default('fastpoke-db'),
+  MONGODB_DATABASE: Joi.string()
+    .allow('', null)
+    .empty(['', null])
+    .default('fastpoke'),
+  MONGODB_PORT: Joi.number().allow('', null).empty(['', null]).default(27017),
+  MONGODB_PARAMS: Joi.string().allow('', null).empty(['', null]).optional(),
 });
